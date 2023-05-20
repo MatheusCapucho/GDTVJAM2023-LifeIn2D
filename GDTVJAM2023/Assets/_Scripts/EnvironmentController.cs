a5a3abf98ab8e44eab8da49624fe24e6cc1e8a24
MatheusCapucho <matheusfcapucho@gmail.com>
2023-05-20 02:55:13 -0300

    Environment Controller

---
+++ GDTVJAM2023/Assets/_Scripts/EnvironmentController.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvironmentController : MonoBehaviour
+{
+    private GameObject _mainScene;
+    private GameObject _parallelScene;
+    private void Awake()
+    {
+        _mainScene = transform.GetChild(0).gameObject;
+        _parallelScene = transform.GetChild(1).gameObject;
+    }
+    private void OnEnable()
+    {
+        MapChanger.ChangeMap += OnMapChange;
+    }
+    private void OnDisable()
+    {
+        MapChanger.ChangeMap -= OnMapChange;
+    }
+
+    private void OnMapChange()
+    {
+        if(_mainScene.activeInHierarchy)
+        {
+            _mainScene.SetActive(false);
+            _parallelScene.SetActive(true);
+        } 
+        else
+        { 
+            _mainScene.SetActive(true);
+            _parallelScene.SetActive(false);
+            
+        }
+    }
+}
--- GDTVJAM2023/Assets/_Scripts/MapChanger.cs
+++ GDTVJAM2023/Assets/_Scripts/MapChanger.cs
@@ -1,18 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MapChanger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static Action ChangeMap;
+
+    [SerializeField]
+    private float _changerCooldown = 10f;
+    private float _timeElapsed = 0f;
+
+    private void Update()
     {
-        
+        _timeElapsed += Time.deltaTime;
+
+        if(Input.GetKeyDown(KeyCode.K) && _timeElapsed > _changerCooldown)
+        {
+            _timeElapsed = 0f;
+            ChangeMap?.Invoke();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
